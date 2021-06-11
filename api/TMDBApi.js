896a3ec8667e6096e45ac3e4d070197f584a8489
JeuFore <37927640+JeuFore@users.noreply.github.com>
2021-06-11 18:46:12 +0200

    Update TMDBApi.js

--- api/TMDBApi.js
+++ api/TMDBApi.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const API_TOKEN = '058801795ffa633705abd5a68808ba59';
+const API_TOKEN = '';
 
 export async function getFilmsFromApi(req) {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_TOKEN}&language=fr&query=${req}`
